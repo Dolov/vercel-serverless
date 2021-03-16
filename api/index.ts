a7db26b9bb32c523dc0b6e9f8c6259c2850ebbbc
shisongyan <songyan_shi@163.com>
2021-03-16 16:41:27 +0800

    ss

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,6 @@
-module.exports = (req, res) => {
-	const { name = 'World' } = req.query
-	res.status(200).send(`Hello ${name}!`)
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { name = 'World' } = request.query
+  response.status(200).send(`Hello ${name}!`)
 }
