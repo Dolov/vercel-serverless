9c13520c26fe46c89fdc58b95ae40ae05170ccd8
shisongyan <songyan_shi@163.com>
2021-03-16 17:56:12 +0800

    基本工鞥

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,52 @@
+const https = require('https') 
 import { VercelRequest, VercelResponse } from '@vercel/node'
+import cheerio from 'cheerio'
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`)
+const fetchUrlContent = (url, headers?) => {
+  return new Promise((resolve) => {
+    https.get(url, {
+      headers,
+    }, (res) => {
+      let html = ''
+  
+      res.on('data', (data) => {
+        html += data
+      })
+  
+      res.on('end', () => {
+        resolve(html)
+      })
+    })
+  })
+}
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  const { q } = request.query
+	if (!q) {
+		response.status(200).send('nothing')
+	}
+	const html = await fetchUrlContent(`https://www.npmjs.com/package/${q}`)
+	const $ = cheerio.load(html)
+
+	console.log('开始解析')
+	const responseJson: {
+		Homepage?: string;
+		Repository?: string;
+		Github1s?: string;
+	} = {
+
+	}
+	$('h3').each(function() {
+		if ($(this).text() === 'Homepage') {
+			const Homepage = $(this).parent().find('a').attr('href')
+			responseJson.Homepage = Homepage
+		}
+
+		if ($(this).text() === 'Repository') {
+			const Repository = $(this).parent().find('a').attr('href')
+			responseJson.Repository = Repository
+			responseJson.Github1s = Repository.replace('github', 'github1s')
+		}
+	})
+  response.status(200).send(responseJson)
 }
